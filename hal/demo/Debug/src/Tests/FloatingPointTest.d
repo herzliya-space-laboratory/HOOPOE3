64b4e0750396f32a64ad143e6024f2af2cea89e9
Adi Rabinovitz <adiwwww@gmail.com>
2019-07-21 14:04:49 +0300

    Comment gitIgnore
    Comment all file types

---
+++ hal/demo/Debug/src/Tests/FloatingPointTest.d
@@ -0,0 +1,4 @@
+src/Tests/FloatingPointTest.d: ../src/Tests/FloatingPointTest.c \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/boolean.h
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/boolean.h:
---
+++ hal/demo/Debug/src/Tests/boardTest.d
@@ -0,0 +1,90 @@
+src/Tests/boardTest.d: ../src/Tests/boardTest.c ../src/Tests/ADCtest.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/boolean.h \
+ ../src/Tests/SDCardTest.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/FreeRTOS.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/projdefs.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/FreeRTOSConfig.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/boards/ISIS_OBC_G20/board.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/utility/trace.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/peripherals/dbgu/dbgu.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/peripherals/pio/pio.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/utility/assert.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/portable.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/portable/MemMang/standardMemMang.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/mpu_wrappers.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/task.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/list.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/I2C.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/commons.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/semphr.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/queue.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/LED.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/PWM.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/SPI.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/ADC.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Timing/RTC.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Timing/Time.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Storage/FRAM.h \
+ C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/supervisor.h
+
+../src/Tests/ADCtest.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/boolean.h:
+
+../src/Tests/SDCardTest.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/FreeRTOS.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/projdefs.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/FreeRTOSConfig.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/boards/ISIS_OBC_G20/board.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/utility/trace.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/peripherals/dbgu/dbgu.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/peripherals/pio/pio.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/utility/assert.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/portable.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/portable/MemMang/standardMemMang.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/mpu_wrappers.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/task.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/list.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/I2C.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//at91/include/at91/commons.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/semphr.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//freertos/include/freertos/queue.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/LED.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/PWM.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/SPI.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Drivers/ADC.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Timing/RTC.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Timing/Time.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/Storage/FRAM.h:
+
+C:/Users/user1/Documents/hoopoe-2/hal/demo/..//hal/include/hal/supervisor.h:
