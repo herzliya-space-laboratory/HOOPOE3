b4bd2339ae7d18a694ccdb35f42088644389749c
tomerna2 <53230417+tomerna2@users.noreply.github.com>
2019-08-12 15:17:24 +0300

    Updated all ADCS commands in splTypes
    Added all the commands TC and TM to the splTypes
    updeted the return tlm naming in commands file

--- satellite-subsystems/demo/src/sub-systemCode/ADCS/AdcsCommands.c
+++ satellite-subsystems/demo/src/sub-systemCode/ADCS/AdcsCommands.c
@@ -118,6 +118,7 @@ typedef enum {
 	MeanAnomaly 	= 52,
 	Epoch 			= 53
 }Sgp4OrbitParam;
+
 int SetSgp4OrbitParams(Sgp4OrbitParam params, unsigned char data[CSPACE_ADCS_SG4ORBITPARAMS_SIZE])
 {
 	if(NULL == data){
@@ -146,12 +147,12 @@ int SetRateSensorOffset(unsigned char *data)
 
 }
 
-void SendAckWithInfo(byte *info,unsigned int length, int subType){
+void SendAdcsTlm(byte *info, unsigned int length, int subType, boolean isOnlineTelem){
 	TM_spl tm;
 	time_unix time_now;
 	Time_getUnixEpoch(&time_now);	//get time
 	tm.time = time_now;
-	tm.type = TC_ADCS_T;
+	tm.type = TM_ADCS_T;
 	tm.subType = subType;
 	memcpy(tm.data,info,length);
 	tm.length = length;
@@ -184,231 +185,230 @@ TroubleErrCode AdcsExecuteCommand(TC_spl *cmd)
 	cspace_adcs_estmode_sel att;
 	switch(sub_type)
 	{
+		case ADCS_GENRIC_ST:
+			err = AdcsGenericI2cCmd(cmd->data,cmd->length,&rv);
+			//TODO: log 'rv'. maybe send it in ACK form
+			SendAdcsTlm(&rv,siseof(int), ADCS_GENRIC_ST, FALSE);
+			break;
+		case ADCS_SET_BOOT_INDEX_ST:
+			memcpy(&bootindex,cmd->data,sizeof(bootindex));
+			err = cspaceADCS_BLSetBootIndex(ADCS_ID,bootindex);
+			break;
 
-	case ADCS_GENRIC_ST:
-		err = AdcsGenericI2cCmd(cmd->data,cmd->length,&rv);
-		//TODO: log 'rv'. maybe send it in ACK form
-		break;
-	case ADCS_SET_BOOT_INDEX_ST:
-		memcpy(&bootindex,cmd->data,sizeof(bootindex));
-		err = cspaceADCS_BLSetBootIndex(ADCS_ID,bootindex);
-		break;
+		case ADCS_RUN_BOOT_PROGRAM_ST:
+			err = cspaceADCS_BLRunSelectedProgram(ADCS_ID);
+			break;
 
-	case ADCS_RUN_BOOT_PROGRAM_ST:
-		err = cspaceADCS_BLRunSelectedProgram(ADCS_ID);
-		break;
+		case ADCS_SET_SGP4_ORBIT_PARAMS_ST:
+			err = SetSgp4OrbitParams(All_sgp4_params, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_PARAMS_ST:
-		err = SetSgp4OrbitParams(All_sgp4_params, cmd->data);
-		break;
+		case ADCS_SET_SGP4_ORBIT_INC_ST:
+			err = SetSgp4OrbitParams(Inclination, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_INC_ST:
-		err = SetSgp4OrbitParams(Inclination, cmd->data);
-		break;
+		case ADCS_SET_SGP4_ORBIT_ECC_ST:
+			err = SetSgp4OrbitParams(Eccentricity, cmd->data);
+			break;
+		case ADCS_SET_SGP4_ORBIT_EPOCH_ST:
+			 err = SetSgp4OrbitParams(Epoch, cmd->data);
+			 return err;
+		case ADCS_SET_SGP4_ORBIT_RAAN_ST:
+			err = SetSgp4OrbitParams(RAAN, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_ECC_ST:
-		err = SetSgp4OrbitParams(Eccentricity, cmd->data);
-		break;
-	case ADCS_SET_SGP4_ORBIT_EPOCH_ST:
-		 err = SetSgp4OrbitParams(Epoch, cmd->data);
-		 return err;
-	case ADCS_SET_SGP4_ORBIT_RAAN_ST:
-		err = SetSgp4OrbitParams(RAAN, cmd->data);
-		break;
+		case ADCS_SET_SGP4_ORBIT_ARG_OF_PER_ST:
+			err = SetSgp4OrbitParams(ArgOfPerigee, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_ARG_OF_PER_ST:
-		err = SetSgp4OrbitParams(ArgOfPerigee, cmd->data);
-		break;
+		case ADCS_SET_SGP4_ORBIT_BSTAR_DRAG_ST:
+			err = SetSgp4OrbitParams(BStardrag, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_BSTAR_DRAG_ST:
-		err = SetSgp4OrbitParams(BStardrag, cmd->data);
-		break;
+		case ADCS_SET_SGP4_ORBIT_MEAN_MOT_ST:
+			err = SetSgp4OrbitParams(MeanMotion, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_MEAN_MOT_ST:
-		err = SetSgp4OrbitParams(MeanMotion, cmd->data);
-		break;
+		case ADCS_SET_SGP4_ORBIT_MEAN_ANOM_ST:
+			err = SetSgp4OrbitParams(MeanAnomaly, cmd->data);
+			break;
 
-	case ADCS_SET_SGP4_ORBIT_MEAN_ANOM_ST:
-		err = SetSgp4OrbitParams(MeanAnomaly, cmd->data);
-		break;
+		case ADCS_RESET_BOOT_REGISTER_ST:
+			err = ResetBootRegisters();
+			break;
 
-	case ADCS_RESET_BOOT_REGISTER_ST:
-		err = ResetBootRegisters();
-		break;
+		case ADCS_SAVE_CONFIG_ST:
+			err = cspaceADCS_saveConfig(ADCS_ID);
+			break;
 
-	case ADCS_SAVE_CONFIG_ST:
-		err = cspaceADCS_saveConfig(ADCS_ID);
-		break;
+		case ADCS_SAVE_ORBIT_PARAM_ST:
+			err = cspaceADCS_saveOrbitParam(ADCS_ID);
+			break;
 
-	case ADCS_SAVE_ORBIT_PARAM_ST:
-		err = cspaceADCS_saveOrbitParam(ADCS_ID);
-		break;
+		case ADCS_SET_UNIX_TIME_ST: //TODO: get unix time
+			err = cspaceADCS_setCurrentTime(ADCS_ID,(cspace_adcs_unixtm_t*)cmd->data);
+			break;
 
-	case ADCS_SET_UNIX_TIME_ST: //TODO: get unix time
-		err = cspaceADCS_setCurrentTime(ADCS_ID,(cspace_adcs_unixtm_t*)cmd->data);
-		break;
+		case ADCS_CACHE_ENABLE_ST:
+			err = cspaceADCS_cacheStateADCS(ADCS_ID, cmd->data[0]);
+			break;
 
-	case ADCS_CACHE_ENABLE_ST:
-		err = cspaceADCS_cacheStateADCS(ADCS_ID, cmd->data[0]);
-		break;
+		case ADCS_DEPLOY_MAG_BOOM_ST:
+			//err = cspaceADCS_deployMagBoomADCS(ADCS_ID,cmd->data[0]);
+		#ifdef TESTING
+			printf("magnetometer not deployed \nBOOM\n");
+		#endif
+			break;
 
-	case ADCS_DEPLOY_MAG_BOOM_ST:
-		//err = cspaceADCS_deployMagBoomADCS(ADCS_ID,cmd->data[0]);
-	#ifdef TESTING
-		printf("magnetometer not deployed \nBOOM\n");
-	#endif
-		break;
+		case ADCS_RUN_MODE_ST:
+			memcpy(&runmode,cmd->data,sizeof(runmode));
+			err = cspaceADCS_setRunMode(ADCS_ID, runmode);
+			break;
 
-	case ADCS_RUN_MODE_ST:
-		memcpy(&runmode,cmd->data,sizeof(runmode));
-		err = cspaceADCS_setRunMode(ADCS_ID, runmode);
-		break;
+		case ADCS_SET_MTQ_CONFIG_ST:
+			err = AdcsI2cCmdWithID(MTQ_CONFIG_CMD_ID,cmd->data,MTQ_CONFIG_CMD_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_SET_MTQ_CONFIG_ST, FALSE);
+			break;
 
-	case ADCS_SET_MTQ_CONFIG_ST:
-		err = AdcsI2cCmdWithID(MTQ_CONFIG_CMD_ID,cmd->data,MTQ_CONFIG_CMD_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_SET_MTQ_CONFIG_ST);
-		break;
+		case ADCS_SET_MAGNETMTR_MOUNT_ST:
+			err = cspaceADCS_setMagMountConfig(ADCS_ID,(cspace_adcs_magmountcfg_t*)cmd->data);
+			break;
 
-	case ADCS_SET_MAGNETMTR_MOUNT_ST:
-		err = cspaceADCS_setMagMountConfig(ADCS_ID,(cspace_adcs_magmountcfg_t*)cmd->data);
-		break;
+		case ADCS_SET_MAGNETMTR_OFFSET_ST:
+			err = cspaceADCS_setMagOffsetScalingConfig(ADCS_ID,(cspace_adcs_offscal_cfg_t*)cmd->data);
+			break;
 
-	case ADCS_SET_MAGNETMTR_OFFSET_ST:
-		err = cspaceADCS_setMagOffsetScalingConfig(ADCS_ID,(cspace_adcs_offscal_cfg_t*)cmd->data);
-		break;
+		case ADCS_SET_MAGNETMTR_SENSTVTY_ST:
+			err = cspaceADCS_setMagSensConfig(ADCS_ID,(cspace_adcs_magsencfg_t*)cmd->data);
+			break;
 
-	case ADCS_SET_MAGNETMTR_SENSTVTY_ST:
-		err = cspaceADCS_setMagSensConfig(ADCS_ID,(cspace_adcs_magsencfg_t*)cmd->data);
-		break;
+		case ADCS_SET_DETUMB_CTRL_PARAM_ST:
+			err = AdcsI2cCmdWithID(DETUMB_CTRL_PARAM_CMD_ID,cmd->data,DETUMB_CTRL_PARAM_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_SET_DETUMB_CTRL_PARAM_ST, FALSE);
+			break;
 
-	case ADCS_SET_DETUMB_CTRL_PARAM_ST:
-		err = AdcsI2cCmdWithID(DETUMB_CTRL_PARAM_CMD_ID,cmd->data,DETUMB_CTRL_PARAM_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_SET_DETUMB_CTRL_PARAM_ST);
-		break;
+		case ADCS_SET_YWHEEL_CTRL_PARAM_ST:
+			err = AdcsI2cCmdWithID(SET_YWHEEL_CTRL_PARAM_CMD_ID,cmd->data,SET_YWHEEL_CTRL_PARAM_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_SET_YWHEEL_CTRL_PARAM_ST, FALSE);
+			break;
 
-	case ADCS_SET_YWHEEL_CTRL_PARAM_ST:
-		err = AdcsI2cCmdWithID(SET_YWHEEL_CTRL_PARAM_CMD_ID,cmd->data,SET_YWHEEL_CTRL_PARAM_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_SET_YWHEEL_CTRL_PARAM_ST);
-		break;
+		case ADCS_SET_RWHEEL_CTRL_PARAM_ST:
+			err = AdcsI2cCmdWithID(SET_RWHEEL_CTRL_PARAM_CMD_ID,cmd->data,SET_RWHEEL_CTRL_PARAM_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_SET_RWHEEL_CTRL_PARAM_ST, FALSE);
+			break;
+		case ADCS_SET_MOMENT_INTERTIA_ST:
+			err = AdcsI2cCmdWithID(SET_MOMENT_INERTIA_CMD_ID,cmd->data,SET_MOMENT_INERTIA_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_SET_MOMENT_INTERTIA_ST, FALSE);
+			break;
+		case ADCS_PROD_INERTIA_ST:
+			err = AdcsI2cCmdWithID(SET_PROD_INERTIA_CMD_ID,cmd->data,12,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_PROD_INERTIA_ST, FALSE);
+			break;
+		case ADCS_ESTIMATION_PARAM1_ST:
+			err =  cspaceADCS_setEstimationParam1(ADCS_ID,(cspace_adcs_estparam1_t*)cmd->data);
+			break;
+		case ADCS_ESTIMATION_PARAM2_ST:
+			err =  cspaceADCS_setEstimationParam2(ADCS_ID,(cspace_adcs_estparam2_t*)cmd->data);
+			break;
+		case ADCS_GET_CURR_UNIX_TIME_ST:
+			err = cspaceADCS_getCurrentTime(ADCS_ID,&time);
+			//TODO: send 'time' as ack or something
+			SendAdcsTlm(time,, FALSE);
+			break;
 
-	case ADCS_SET_RWHEEL_CTRL_PARAM_ST:
-		err = AdcsI2cCmdWithID(SET_RWHEEL_CTRL_PARAM_CMD_ID,cmd->data,SET_RWHEEL_CTRL_PARAM_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_SET_RWHEEL_CTRL_PARAM_ST);
-		break;
-	case ADCS_SET_MOMENT_INTERTIA_ST:
-		err = AdcsI2cCmdWithID(SET_MOMENT_INERTIA_CMD_ID,cmd->data,SET_MOMENT_INERTIA_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_SET_MOMENT_INTERTIA_ST);
-		break;
-	case ADCS_PROD_INERTIA_ST:
-		err = AdcsI2cCmdWithID(SET_PROD_INERTIA_CMD_ID,cmd->data,12,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_PROD_INERTIA_ST);
-		break;
-	case ADCS_ESTIMATION_PARAM1_ST:
-		err =  cspaceADCS_setEstimationParam1(ADCS_ID,(cspace_adcs_estparam1_t*)cmd->data);
-		break;
-	case ADCS_ESTIMATION_PARAM2_ST:
-		err =  cspaceADCS_setEstimationParam2(ADCS_ID,(cspace_adcs_estparam2_t*)cmd->data);
-		break;
-	case ADCS_GET_CURR_UNIX_TIME_ST:
-		err = cspaceADCS_getCurrentTime(ADCS_ID,&time);
-		//TODO: send 'time' as ack or something
-		//SendAckWithInfo(time);
-		break;
+		case ADCS_MTQ_CONFIG_ST:
+			//TODO: what's the difference between this(st = 135) and  sub_typeID = 117
+			break;
 
-	case ADCS_MTQ_CONFIG_ST:
-		//TODO: what's the difference between this(st = 135) and  sub_typeID = 117
-		break;
+		case ADCS_RW_CONFIG_ST:
+			err = AdcsI2cCmdWithID(SET_WHEEL_CONFIG_CMD_ID,cmd->data,SET_WHEEL_CONFIG_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_RW_CONFIG_ST, FALSE);
+			break;
 
-	case ADCS_RW_CONFIG_ST:
-		err = AdcsI2cCmdWithID(SET_WHEEL_CONFIG_CMD_ID,cmd->data,SET_WHEEL_CONFIG_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_RW_CONFIG_ST);
-		break;
+		case ADCS_GYRO_CONFIG_ST:
+			err = AdcsI2cCmdWithID(SET_GYRO_CONFIG_CMD_ID,cmd->data,SET_GYRO_CONFIG_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_GYRO_CONFIG_ST, FALSE);
+			break;
 
-	case ADCS_GYRO_CONFIG_ST:
-		err = AdcsI2cCmdWithID(SET_GYRO_CONFIG_CMD_ID,cmd->data,SET_GYRO_CONFIG_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_GYRO_CONFIG_ST);
-		break;
+		case ADCS_CSS_CONFIG_ST:
+			err = AdcsI2cCmdWithID(SET_CSS_CONFIG_CMD_ID,cmd->data,SET_CSS_CONFIG_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_CSS_CONFIG_ST, FALSE);
+			break;
 
-	case ADCS_CSS_CONFIG_ST:
-		err = AdcsI2cCmdWithID(SET_CSS_CONFIG_CMD_ID,cmd->data,SET_CSS_CONFIG_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_CSS_CONFIG_ST);
-		break;
+		case ADCS_CSS_RELATIVE_SCALE_ST:
+			err = AdcsI2cCmdWithID(SET_CSS_SCALE_CMD_ID,cmd->data,SET_CSS_SCALE_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int),ADCS_CSS_RELATIVE_SCALE_ST, FALSE);
+			break;
 
-	case ADCS_CSS_RELATIVE_SCALE_ST:
-		err = AdcsI2cCmdWithID(SET_CSS_SCALE_CMD_ID,cmd->data,SET_CSS_SCALE_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1,ADCS_CSS_RELATIVE_SCALE_ST);
-		break;
+		case ADCS_CSS_THRESHOLD_ST:
+			// TODO: changes in the entire 272bytes config table(aka stage table)
+			break;
 
-	case ADCS_CSS_THRESHOLD_ST:
-		// TODO: changes in the entire 272bytes config table(aka stage table)
-		break;
+		case ADCS_MAGNETMTR_MOUNT_TRNSFRM_ST:
+			//TODO: check: already done in 'cspaceADCS_setMagMountConfig' in ADCS_SET_MAGNETMTR_MOUNT_ST
+			break;
 
-	case ADCS_MAGNETMTR_MOUNT_TRNSFRM_ST:
-		//TODO: check: already done in 'cspaceADCS_setMagMountConfig' in ADCS_SET_MAGNETMTR_MOUNT_ST
-		break;
+		case ADCS_MAGETMTR_CHNL_1_3_OFFST_ST:
+			break;
 
-	case ADCS_MAGETMTR_CHNL_1_3_OFFST_ST:
-		break;
+		case ADCS_MAGNETMTR_SENS_MATRIX_ST:
+			//TODO: check: already done in 'cspaceADCS_setMagSensConfig' in ADCS_SET_MAGNETMTR_SENSTVTY_ST
+			break;
 
-	case ADCS_MAGNETMTR_SENS_MATRIX_ST:
-		//TODO: check: already done in 'cspaceADCS_setMagSensConfig' in ADCS_SET_MAGNETMTR_SENSTVTY_ST
-		break;
+		case ADCS_RATE_SENSOR_OFFSET_ST:
+			err = AdcsI2cCmdWithID(SET_RATE_SENSOR_CONFIG_CMD_ID,cmd->data,SET_RATE_SENSOR_CONFIG_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_RATE_SENSOR_OFFSET_ST, FALSE);
+			break;
 
-	case ADCS_RATE_SENSOR_OFFSET_ST:
-		err = AdcsI2cCmdWithID(SET_RATE_SENSOR_CONFIG_CMD_ID,cmd->data,SET_RATE_SENSOR_CONFIG_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_RATE_SENSOR_OFFSET_ST);
-		break;
+		case ADCS_RATE_SENSOR_MULT_ST:
+			break;
 
-	case ADCS_RATE_SENSOR_MULT_ST:
-		break;
+		case ADCS_DETUMBLING_GAIN_CONFIG_ST: break;
+		case ADCS_Y_MOMENT_GAIN_CONFIG_ST: break;
+		case ADCS_REF_WHEEL_MOMENTUM_ST: break;
+		case ADCS_RWHEEL_GAIN_ST: break;
+		case ADCS_MOMENT_INERTIA_ST:break;
+		case ADCS_NOISE_CONFIG_ST: break;
 
-	case ADCS_DETUMBLING_GAIN_CONFIG_ST: break;
-	case ADCS_Y_MOMENT_GAIN_CONFIG_ST: break;
-	case ADCS_REF_WHEEL_MOMENTUM_ST: break;
-	case ADCS_RWHEEL_GAIN_ST: break;
-	case ADCS_MOMENT_INERTIA_ST:break;
-	case ADCS_NOISE_CONFIG_ST: break;
+		case ADCS_USE_IN_EKF_ST:
+			err = cspaceADCS_setAttEstMode(ADCS_ID,estmode_full_state_ekf);
+			break;
 
-	case ADCS_USE_IN_EKF_ST:
-		err = cspaceADCS_setAttEstMode(ADCS_ID,estmode_full_state_ekf);
-		break;
+		case ADCS_SET_EST_MODE:
+			memcpy(&att,cmd->data,sizeof(att));
+			err = cspaceADCS_setAttEstMode(ADCS_ID,att);
+			break;
+		case ADCS_GET_FULL_CONFIG_ST:
 
-	case ADCS_SET_EST_MODE:
-		memcpy(&att,cmd->data,sizeof(att));
-		err = cspaceADCS_setAttEstMode(ADCS_ID,att);
-		break;
-	case ADCS_GET_FULL_CONFIG_ST:
+			err = AdcsI2cCmdWithID(GET_ADCS_FULL_CONFIG_CMD_ID,cmd->data,GET_ADCS_FULL_CONFIG_DATA_LENGTH,&rv);
+			//TODO: send ack with 'rv'
+			SendAdcsTlm(&rv,siseof(int), ADCS_GET_FULL_CONFIG_ST, FALSE);
+			break;
+		case ADCS_DUMMY_FUNC:
+			rv = cspaceADCS_getGeneralInfo(ADCS_ID,&data);
+			SendAdcsTlm(&rv,siseof(int),ADCS_DUMMY_FUNC, FALSE);
+			printf("uptime = %d\n",data.fields.uptime_secs);
+			//TODO: change this into a real command and send to GS
+			break;
 
-		err = AdcsI2cCmdWithID(GET_ADCS_FULL_CONFIG_CMD_ID,cmd->data,GET_ADCS_FULL_CONFIG_DATA_LENGTH,&rv);
-		//TODO: send ack with 'rv'
-		SendAckWithInfo(&rv,1, ADCS_GET_FULL_CONFIG_ST);
-		break;
-	case ADCS_DUMMY_FUNC:
-		rv = cspaceADCS_getGeneralInfo(ADCS_ID,&data);
-		SendAckWithInfo(&rv,1,ADCS_DUMMY_FUNC);
-		printf("uptime = %d\n",data.fields.uptime_secs);
-		//TODO: change this into a real command and send to GS
-		break;
-
-	case ADCS_GET_TLM_BY_ID_ST:
-		AdcsI2cCmdReadTLM(cmd->data[0],buffer,cmd->data[1],&rv);	//TODO: add to command dictionary
-		SendAckWithInfo((byte*)buffer,cmd->data[1],cmd->data[0]);
-		break;
-default:
-		//TODO: return unknown cmd
-		break;
+		case ADCS_GET_TLM_BY_ID_ST:
+			AdcsI2cCmdReadTLM(cmd->data[0],buffer,cmd->data[1],&rv);	//TODO: add to command dictionary
+			SendAdcsTlm((byte*)buffer,cmd->data[1],cmd->data[0], FALSE);
+			break;
+		default:
+			//TODO: return unknown subtype
+			break;
 	}
 	return err;
 }
-
