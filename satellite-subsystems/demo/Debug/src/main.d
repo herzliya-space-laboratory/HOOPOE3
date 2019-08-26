68efa5d4c0c5d6cf87bf454a2e13611d0ee7c7f7
Picaso127 <elaynaftaly@gmail.com>
2019-08-26 14:36:52 +0300

    Updated TLM_managment
    Commit Suicide

--- satellite-subsystems/demo/Debug/src/main.d
+++ satellite-subsystems/demo/Debug/src/main.d
@@ -125,6 +125,65 @@ C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//
 
 C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Storage/FRAM.h:
 
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/projdefs.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/FreeRTOSConfig.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/utility/trace.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/peripherals/dbgu/dbgu.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/peripherals/pio/pio.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/utility/assert.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/portable.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/mpu_wrappers.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/task.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/list.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/semphr.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/freertos/include/freertos/queue.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/version/version.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/utility/exithandler.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/commons.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/at91/include/at91/peripherals/cp15/cp15.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Utility/util.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Timing/WatchDogTimer.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/boolean.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Timing/Time.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Drivers/LED.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Drivers/I2C.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Drivers/SPI.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/version/version.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/Storage/FRAM.h:
+>>>>>>> Stashed changes
+
 ../src/sub-systemCode/COMM/GSC.h:
 
 ../src/sub-systemCode/COMM/../Global/Global.h:
@@ -154,6 +213,23 @@ C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//sat
 C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/cspaceADCS_types.h:
 
 C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/errors.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/IsisAntS.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/GomEPS.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/SCS_Gecko/gecko_use_cases.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/SCS_Gecko/gecko_driver.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/cspaceADCS.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/cspaceADCS_types.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/..//satellite-subsystems/include/satellite-subsystems/cspaceADCS_types.h:
+
+C:/Users/97250/Documents/GitHub/online/HOOPOE3/satellite-subsystems/demo/../..//hal/hal/include/hal/errors.h:
+>>>>>>> Stashed changes
 
 ../src/sub-systemCode/Main/../COMM/GSC.h:
 
