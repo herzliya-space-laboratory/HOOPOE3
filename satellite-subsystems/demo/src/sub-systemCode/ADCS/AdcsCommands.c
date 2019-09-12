bed40e431930fd75c6b0d9cf25f15d7fde931950
Eitan Shlisel <eitan.shlisel@gmail.com>
2019-09-12 20:08:41 +0200

    ADCS_GET_CONFIG_PARAM_ST

--- satellite-subsystems/demo/src/sub-systemCode/ADCS/AdcsCommands.c
+++ satellite-subsystems/demo/src/sub-systemCode/ADCS/AdcsCommands.c
@@ -17,7 +17,7 @@
 #include "AdcsCommands.h"
 #include "AdcsMain.h"
 
-
+#define MAX_CONFIG_PARAM_LENGTH 64
 
 #ifdef TESTING
 	#define PRINT_ON_TEST(msg,err)	printf(#msg " = %d\n",err);
@@ -657,7 +657,7 @@ TroubleErrCode AdcsExecuteCommand(TC_spl *cmd)
 			i2c_cmd.id = GET_ADCS_FULL_CONFIG_CMD_ID;
 			i2c_cmd.length = ADCS_FULL_CONFIG_DATA_LENGTH;
 			err = AdcsGenericI2cCmd(&i2c_cmd);
-			SendAdcsTlm(&(i2c_cmd.data[(cmd->data[0]<<8) + cmd->data[1]]),cmd->data[2],ADCS_GET_ADCS_CONFIG_PARAM_ST);
+			SendAdcsTlm(&(i2c_cmd.data[(cmd->data[0]<<8) + cmd->data[1]]),MAX_CONFIG_PARAM_LENGTH,ADCS_GET_ADCS_CONFIG_PARAM_ST);
 			break;
 		case ADCS_GET_TLM_OVERRIDE_FLAG_ST:
 			err = AdcsGetTlmOverrideFlag((Boolean*)&temp);
