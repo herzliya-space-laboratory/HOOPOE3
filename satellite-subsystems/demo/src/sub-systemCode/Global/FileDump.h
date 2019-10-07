1945afc94f9f0f9bb4eaaf62d5c95ec4b142e023
Roy Harel <roy.harel06@gmail.com>
2019-10-07 12:19:00 +0300

    Added Download File By Name Command

---
+++ satellite-subsystems/demo/src/sub-systemCode/Global/FileDump.h
@@ -0,0 +1,16 @@
+/*
+ * FileDump.h
+ *
+ *  Created on: Oct 7, 2019
+ *      Author: Roy's Laptop
+ */
+
+#ifndef FILEDUMP_H_
+#define FILEDUMP_H_
+
+#include "../COMM/splTypes.h"
+#include "../COMM/GSC.h"
+
+void DownloadFile(TC_spl command);
+
+#endif /* FILEDUMP_H_ */
